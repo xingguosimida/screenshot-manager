f282f8800dba30b761aadb6e0df79f92e2b75589
xingguosimida <xingguosimida@gmail.com>
2023-05-28 10:49:17 +0800

    Version 0.1.0

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {Editor, EditorPosition, MarkdownFileInfo, MarkdownView, Plugin, TAbstractFile, TFile} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const fs = require('fs');
+const Diff = require('diff');
+
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -11,75 +13,99 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: MyPluginSettings
+	screenShotPath: string
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(this.app.vault.on('create', (file: TAbstractFile) => this.newFileHandler(file)));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerEvent(this.app.vault.on('delete', (file: TAbstractFile) => this.deleteFileHandler(file)));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.registerEvent(this.app.vault.on('rename', (file: TAbstractFile, oldPath: string): void => {
+			this.newFileHandler(file)
+			let resourceFolder: TAbstractFile | null = this.app.vault.getAbstractFileByPath(this.resourcePathBy(oldPath))
+			if (resourceFolder) {
+				this.app.vault.trash(resourceFolder, true)
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerEvent(this.app.workspace.on("editor-change", this.onChange.bind(this)))
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async onChange(editor: Editor, info: MarkdownView | MarkdownFileInfo) {
+		const pos: EditorPosition = editor.getCursor()
+		const line: string = editor.getLine(pos.line)
+		console.log(line)
+		if (decodeURIComponent(line.trim()).startsWith("![](Pasted image")) {
+			const orig: string | undefined = line.trim().replace("![[", "").replace("]]", "").split("|").first()
+			if (orig) {
+				const replacedString: string = line.replace(/\(.*\)/, `(${this.screenShotPath})`)
+				editor.setLine(pos.line, replacedString)
+				this.screenShotPath = ''
+			}
 
+		}
+	}
+
+	newFileHandler(file: TAbstractFile) {
+		if (this.markdownFile(file)) {
+			let resourcePath: string = this.resourcePath(file)
+			this.app.vault.createFolder(resourcePath)
+		}
+
+		if (this.pngFile(file)) {
+			let attachmentFolderPath = this.appConfiguration().attachmentFolderPath;
+
+			let attachmentFolder: TAbstractFile | null = this.app.vault.getAbstractFileByPath(attachmentFolderPath)
+			if (attachmentFolder) {
+				// @ts-ignore
+				const screenShot = attachmentFolder.children.find(tFile => "png" == tFile.extension)
+				let currentFile: TFile | null = this.app.workspace.getActiveFile()
+				if (currentFile) {
+					let resourceFolderOfCurrentFile: string = this.resourcePath(currentFile)
+					// @ts-ignore
+					const existedFile = this.app.vault.getAbstractFileByPath(resourceFolderOfCurrentFile).children.find(tFile => tFile.name == file.name)
+					if (!existedFile) {
+						this.app.vault.rename(screenShot, `${resourceFolderOfCurrentFile}/${this.screenShotName()}`)
+						this.screenShotPath = `${resourceFolderOfCurrentFile}/${this.screenShotName()}`;
+					}
+				}
+			}
+		}
+	}
+
+	deleteFileHandler(file: TAbstractFile): void {
+		if (this.markdownFile(file)) {
+			let resourcePath: string = this.resourcePath(file)
+			let resourceFolder: TAbstractFile | null = this.app.vault.getAbstractFileByPath(resourcePath)
+			if (resourceFolder) {
+				this.app.vault.delete(resourceFolder, true)
+			}
+		}
+	}
+
+	markdownFile(file: TAbstractFile): boolean {
+		return file.path.endsWith('.md')
+	}
+
+	pngFile(file: TAbstractFile): boolean {
+		return file.path.endsWith('.png')
+	}
+
+	resourcePath(file: TAbstractFile): string {
+		return this.resourcePathBy(file.path)
+	}
+
+	resourcePathBy(filePath: string): string {
+		return `resources/${filePath.substring(0, filePath.indexOf('.')).trim()}`;
+	}
+
+
+	onunload() {
+		this.unload()
 	}
 
 	async loadSettings() {
@@ -89,49 +115,25 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	appConfiguration() {
+		// @ts-ignore
+		let vaultPath = this.app.vault.adapter.basePath
+		let configDir: string = this.app.vault.configDir;
+		const absoluteConfigPath: string = `${vaultPath}/${configDir}/app.json`
+		const fileContent = fs.readFileSync(absoluteConfigPath, 'utf-8');
+		return JSON.parse(fileContent);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	screenShotName() {
+		const currentDate: Date = new Date();
+		const year: number = currentDate.getFullYear();
+		const month: string = String(currentDate.getMonth() + 1).padStart(2, "0");
+		const day: string = String(currentDate.getDate()).padStart(2, "0");
+		const hours: string = String(currentDate.getHours()).padStart(2, "0");
+		const minutes: string = String(currentDate.getMinutes()).padStart(2, "0");
+		const seconds: string = String(currentDate.getSeconds()).padStart(2, "0");
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		return `${year}-${month}-${day}-${hours}-${minutes}-${seconds}.png`;
 	}
 }
